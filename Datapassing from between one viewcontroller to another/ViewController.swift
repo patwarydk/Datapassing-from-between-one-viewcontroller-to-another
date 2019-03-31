9738a5ff67739d7c83d7582b44c2575e318d3bc2
Mohsin Patwary <mohsin.eng@icloud.com>
2019-03-31 12:47:59 +0600

    Initial Commit

---
+++ Datapassing from between one viewcontroller to another/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Datapassing from between one viewcontroller to another
+//
+//  Created by Mohsin Patwary on 31/3/19.
+//  Copyright © 2019 Mohsin Patwary. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
